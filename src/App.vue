6eb90f04b6685ceeee6316148223141a70ed5a4d
MatheusEli <matheuselisouzaferreira@hotmail.com>
2022-11-01 17:18:06 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <q-bar>
+      <q-btn dense flat :icon="fabApple" />
+      <div class="text-weight-bold">
+        App
+      </div>
+      <div class="cursor-pointer gt-md">File</div>
+      <div class="cursor-pointer gt-md">Edit</div>
+      <div class="cursor-pointer gt-md">View</div>
+      <div class="cursor-pointer gt-md">Window</div>
+      <div class="cursor-pointer gt-md">Help</div>
+      <q-space />
+      <q-btn dense flat icon="airplay" class="gt-xs" />
+      <q-btn dense flat icon="battery_charging_full" />
+      <q-btn dense flat icon="wifi" />
+      <div>9:41</div>
+      <q-btn dense flat icon="search" />
+      <q-btn dense flat icon="list" />
+    </q-bar>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
